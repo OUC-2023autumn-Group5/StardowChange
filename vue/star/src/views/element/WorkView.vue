f7555e464560afe40c081665f9a08271f30ce864
djk <111996441+djk0309@users.noreply.github.com>
2023-11-26 15:51:08 +0800

    优化界面

--- vue/star/src/views/element/WorkView.vue
+++ vue/star/src/views/element/WorkView.vue
@@ -1,6 +1,8 @@
 
 <template>
- <el-upload
+ 
+<div>
+  <el-upload
   class="upload-demo"
   ref="upload"
   action="https://jsonplaceholder.typicode.com/posts/"
@@ -12,13 +14,28 @@
   <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
   <el-button style="margin-left: 10px;" size="small" type="primary" ><router-link to="/" class="no">返回上一个界面</router-link></el-button>
   <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+
 </el-upload>
+<div class="image__preview">
+  <el-image 
+    style="width: 100px; height: 100px"
+    :src="url" 
+    :preview-src-list="srcList">
+  </el-image>
+</div>
+</div>
+
 </template>
 <script>
   export default {
     data() {
       return {
-        fileList: []
+        fileList: [],
+        url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+        srcList: [
+          'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
+          'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg'
+        ]
       };
     },
     methods: {
@@ -33,6 +50,7 @@
       }
     }
   }
+  
 </script>
 
 <style >
@@ -40,4 +58,8 @@
     text-decoration:none;
     color: white;
   }
+  .image__preview{
+    margin-top: 50px;
+  }
+
 </style>
