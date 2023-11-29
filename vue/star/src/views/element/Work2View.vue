bed3b32018e72e530126e1fe3a8402ff6628ca18
21020007039 <145846757+21020007039@users.noreply.github.com>
2023-11-29 17:17:41 +0800

    vue更新

--- vue/star/src/views/element/Work2View.vue
+++ vue/star/src/views/element/Work2View.vue
@@ -1,11 +1,10 @@
-
 <template>
  
 <div>
   <el-upload
   class="upload-demo"
   ref="upload"
-  action="http://127.0.0.1:5000/upload"
+  action="http://127.0.0.1:5000/work"
   multiple
   :on-success="handleAvatarSuccess"
   :on-preview="handlePreview"
@@ -13,9 +12,9 @@
   :file-list="fileList"
   :auto-upload="false">
   <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-  <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传第一个文件</el-button>
-  <el-button style="margin-left: 10px;" size="small" type="primary" ><router-link to="/work" class="no">上传第二个文件</router-link></el-button> 
-  <el-button style="margin-left: 10px;" size="small" type="primary" ><router-link to="/" class="no">返回上一个界面</router-link></el-button>
+  <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传第二个文件</el-button>
+  <el-button style="margin-left: 10px;" size="small" type="primary" ><router-link to="/result" class="no">查看结果</router-link></el-button> 
+  <el-button style="margin-left: 10px;" size="small" type="primary" ><router-link to="/vue" class="no">返回上一个界面</router-link></el-button>
   <div slot="tip" class="el-upload__tip">只能上传第一个jpg/png文件，且不超过500kb</div>
   </el-upload>
 </div>
