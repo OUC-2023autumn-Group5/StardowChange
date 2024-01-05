979993efc23fcf86e017a06c46d3dab052f2f28e
王 星苹 <2924368393@qq.com>
2024-01-05 21:15:34 +0800

    添加注释，整理代码结构，调整代码

--- model/app.py
+++ model/app.py
@@ -1,61 +1,57 @@
-
 from flask_cors import CORS
 from flask import request
-from PIL import Image
-from flask import render_template
 from flask import Flask,send_file
+import os
 import sys
 sys.path.append("../")
 from test import predict
 
-
- 
 app = Flask(__name__)
 cors = CORS(app)
 
+# 临时文件路径
+img1 = 'tmp/before.bmp'
+img2 = 'tmp/after.bmp'
+path = 'tmp/result.png'
+
+# No such directory处理
+def mkdir(dir):
+    if(not os.path.exists(dir)):
+        os.mkdir(dir)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
- 
+    return 'This is a white page!'
+
 @app.route("/upload1", methods=['post', 'get'])
 def upload1():
-
+    # 暂存上传的before.bmp文件
     f = request.files['file']
     print(f.filename)
-    f.save('before.bmp')
-    # # image_file = Image.open(f.filename)  # open colour image
-    # # image_file = image_file.convert('1')  # convert image to black and white
-    # # path = '../vue/star/public/result.png'//需要保存到的路径
-    # # image_file.save(path)
+    f.save(img1)
     print('success')
     return 'success'
 
 @app.route("/upload2", methods=['post', 'get'])
 def upload2():
-
+    # 暂存上传的after.bmp文件
     f = request.files['file']
     print(f.filename)
-    f.save('after.bmp')
-    # # image_file = Image.open(f.filename)  # open colour image
-    # # image_file = image_file.convert('1')  # convert image to black and white
-    # # path = '../vue/star/public/result.png'//需要保存到的路径
-    # # image_file.save(path)
+    f.save(img2)
     print('success')
     return 'success'
 
 @app.route("/work", methods=['post','get'])
 def work():
-    image_path = 'result.png'
     if request.method == 'POST':
-        path = './result.png'
-        predict("before.bmp","after.bmp","net-6.pt",path)
+        # 调用模型开始分析
+        predict(img1, img2, "net-8.pt", path)
     elif request.method == 'GET':
-        image_path = 'result.png'
         print('success')
     # 返回 BMP 图片
-    return send_file(image_path, mimetype='image/png')
+    return send_file(path, mimetype='image/png')
     # return 'success'
 
 if __name__ == '__main__':
+    mkdir('tmp')
     app.run()
